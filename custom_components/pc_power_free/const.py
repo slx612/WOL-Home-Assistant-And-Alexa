fc25ed7787a350bf898afab9ad173b14cd0e5f68
slx612 <39024996+slx612@users.noreply.github.com>
2026-04-29 23:23:25 +0200

    Fix Home Assistant integration source sync for beta.5

--- custom_components/pc_power_free/const.py
+++ custom_components/pc_power_free/const.py
@@ -6,14 +6,16 @@ CONF_AGENT_PORT = "agent_port"
 CONF_API_TOKEN = "api_token"
 CONF_BROADCAST_ADDRESS = "broadcast_address"
 CONF_BROADCAST_PORT = "broadcast_port"
+CONF_CAPABILITIES = "capabilities"
 CONF_DISCOVERY_SUBNETS = "discovery_subnets"
 CONF_MACHINE_ID = "machine_id"
+CONF_PLATFORM = "platform"
 CONF_SCAN_INTERVAL = "scan_interval"
 
 DISCOVERY_CACHE = "discoveries"
 MANUAL_DISCOVERY_OPTION = "__manual__"
 
-DEFAULT_AGENT_PORT = 8777
+DEFAULT_AGENT_PORT = 58477
 DEFAULT_BROADCAST_ADDRESS = "255.255.255.255"
 DEFAULT_BROADCAST_PORT = 9
 DEFAULT_DISCOVERY_SUBNETS = ""
@@ -23,6 +25,7 @@ ZEROCONF_SERVICE_TYPE = "_pcpowerfree._tcp.local."
 
 STATUS_AGENT_VERSION = "agent_version"
 STATUS_BOOTED_AT = "booted_at"
+STATUS_CAPABILITIES = "capabilities"
 STATUS_COMMAND_GUARD_ACTIVE = "command_guard_active"
 STATUS_COMMAND_GUARD_MODE = "command_guard_mode"
 STATUS_COMMAND_GUARD_UNTIL_TS = "command_guard_until_ts"
@@ -33,5 +36,6 @@ STATUS_LAST_COMMAND_AT = "last_command_at"
 STATUS_MAC_ADDRESSES = "mac_addresses"
 STATUS_MACHINE_ID = "machine_id"
 STATUS_ONLINE = "online"
+STATUS_PLATFORM = "platform"
 STATUS_REACHABLE = "reachable"
 STATUS_UPTIME_SECONDS = "uptime_seconds"
